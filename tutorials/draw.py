0360c9460e9807ea2fefff494c8414a4727c36af
Kiran Maheriya <maheriya@vasuyantra.com>
2018-06-15 12:49:54 -0700

    Removed unwanted comments

--- tutorials/draw.py
+++ tutorials/draw.py
@@ -14,14 +14,6 @@ while (1):
     d+=1
     d = (d%360)
     ##print("r={}, d={}".format(r,d))
-#    if r == 255:
-#        inc = False
-#    elif r == 20:
-#        inc = True            
-#    if inc:
-#        r+=1
-#    else:
-#        r-=1
     cv.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
     cv.line(img, (511, 0), (0, 511), (255, 0, 0), 5)
     cv.rectangle(img, (384, 0), (510, 128), (0, 255, 0), 3)

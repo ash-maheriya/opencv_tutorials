c4c8c4f70e1066dad1b975cd2092e1149ce86da6
Kiran Maheriya <maheriya@vasuyantra.com>
2018-07-06 15:58:53 -0700

    Adding ipython notebook and some other files

--- tutorials/mouse_advanced.py
+++ tutorials/mouse_advanced.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2 as cv
 drawing = False # true if mouse is pressed
 mode = True # if True, draw rectangle. Press 'm' to toggle to curve
+color = True # if True, use green. Press 'c' to toggle to red
 ix,iy = -1,-1
-ox, oy = 0, 0
 
 img = np.zeros((512,512,3), np.uint8)
 oimg = np.zeros((512,512,3), np.uint8)
@@ -17,21 +17,22 @@ def draw_circle(event,x,y,flags,param):
     if event == cv.EVENT_LBUTTONDOWN:
         drawing = True
         ix,iy = x,y
-        ox, oy = ix, iy
         oimg[:] = img 
     elif event == cv.EVENT_MOUSEMOVE:
         if drawing == True:
             if mode == True:
                 img[:] = oimg
                 cv.rectangle(img,(ix,iy),(x,y),(0, 0, 255), 1)
-                ox, oy = x, y
             else:
                 cv.circle(img,(x,y),5,(0,0,255),-1)
     elif event == cv.EVENT_LBUTTONUP:
         drawing = False
         if mode == True:
             img[:] = oimg
-            cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+            if color == True:
+                cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+            else:
+                cv.rectangle(img,(ix,iy),(x,y),(255,0,0),-1)
         else:
             cv.circle(img,(x,y),5,(0,0,255),-1)
 
@@ -42,6 +43,8 @@ while(1):
     k = cv.waitKey(1) & 0xFF
     if k == ord('m'):
         mode = not mode
+    elif k == ord('c'):
+        color = not color
     elif k == 27:
         break
 cv.destroyAllWindows()
